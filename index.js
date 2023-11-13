0881865381500e782b4680aa5b8d28dea8f51481
Kevin <kevin.macias.a@gmail.com>
2023-11-13 11:53:08 -0800

    Add toggle behavior to menu button in index.js/html

--- index.js
+++ index.js
@@ -16,11 +16,32 @@ document.documentElement.setAttribute("color-theme", theme);
 
 const themeButton = document.querySelector(".theme-button");
 
-themeButton.addEventListener("click", () => {
+const themeHanlder = () => {
   const currentTheme = document.documentElement.getAttribute("color-theme");
   const swapTheme = currentTheme === "dark" ? "light" : "dark";
   document.documentElement.setAttribute("color-theme", swapTheme);
   updateLocalStorage("colorTheme", swapTheme);
+};
+
+const submenuButton = document.querySelector(".mobile-button__hamburger");
+const submenuHanlder = (state) => {
+  if (state === undefined) submenuButton.toggleAttribute("active");
+  else {
+    if (state) submenuButton.setAttribute("active");
+    else submenuButton.removeAttribute("active");
+  }
+};
+
+document.addEventListener("click", (event) => {
+  const target = event.target;
+  const button = target.closest("button");
+
+  if (button === themeButton) themeHanlder();
+
+  if (button === submenuButton) submenuHanlder();
+  else {
+    submenuHanlder(false);
+  }
 });
 
 function updateLocalStorage(key, value) {
