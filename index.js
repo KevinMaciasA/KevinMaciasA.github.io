f65d7cb474e2d0ddfd57028cf748cde209eff286
Kevin <kevin.macias.a@gmail.com>
2023-11-13 18:10:59 -0800

    Add functionality to all buttons of the site

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ if (!storage) {
 }
 document.documentElement.setAttribute("color-theme", theme);
 
-const themeButton = document.querySelector(".theme-button");
+const resumeButton = document.querySelector(".button__resume");
+const contactButton = document.querySelector(".button__contact");
 
+const themeButton = document.querySelector(".theme-button");
 const themeHanlder = () => {
   const currentTheme = document.documentElement.getAttribute("color-theme");
   const swapTheme = currentTheme === "dark" ? "light" : "dark";
@@ -36,6 +38,9 @@ document.addEventListener("click", (event) => {
   const target = event.target;
   const button = target.closest("button");
 
+  if (button === resumeButton) scrollToSection("info");
+  if (button === contactButton) scrollToSection("find-me");
+
   if (button === themeButton) themeHanlder();
 
   if (button === submenuButton) submenuHanlder();
@@ -51,4 +56,10 @@ function updateLocalStorage(key, value) {
   let newStorage = { ...storage };
   newStorage[key] = value;
   localStorage.setItem(storageName, JSON.stringify(newStorage));
+}
+
+function scrollToSection(id) {
+  let section = document.getElementById(id);
+
+  if (section) window.scrollTo(0, section.offsetTop);
 }
