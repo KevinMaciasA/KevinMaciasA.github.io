5409d7f3533855f5c937df4d1484b386ffe77b61
Kevin <kevin.macias.a@gmail.com>
2023-11-15 13:29:32 -0800

    Create handleTheme.js to separete theme logic from index.js

---
+++ scripts/handleTheme.js
@@ -0,0 +1,27 @@
+import { updateLocalStorage, storageName } from "./utils/localStorage";
+
+const storageString = localStorage.getItem(storageName);
+const storage = JSON.parse(storageString);
+let theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+  ? "dark"
+  : "light";
+
+if (!storage) {
+  const newStorage = {
+    colorTheme: theme,
+  };
+  localStorage.setItem(storageName, JSON.stringify(newStorage));
+} else {
+  theme = storage.colorTheme ?? theme;
+}
+document.documentElement.setAttribute("color-theme", theme);
+
+const themeButton = document.querySelector(".theme-button");
+const themeHanlder = () => {
+  const currentTheme = document.documentElement.getAttribute("color-theme");
+  const swapTheme = currentTheme === "dark" ? "light" : "dark";
+  document.documentElement.setAttribute("color-theme", swapTheme);
+  updateLocalStorage("colorTheme", swapTheme);
+};
+
+export { themeHanlder, themeButton };
