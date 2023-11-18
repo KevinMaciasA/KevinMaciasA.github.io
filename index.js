17c2da5816fa2c30239f059322a3b874a0d20524
Kevin <kevin.macias.a@gmail.com>
2023-11-17 18:04:53 -0800

    Add buttons animations in cards & across the page

--- index.js
+++ index.js
@@ -28,6 +28,23 @@ document.addEventListener("click", (event) => {
   }
 });
 
+const animateBtn = (card) => {
+  const btn = card.querySelector(".button__card");
+  btn.addEventListener("animationend", () => {
+    btn.classList.remove("animate");
+  });
+  btn.classList.add("animate");
+};
+document.addEventListener("click", (event) => {
+  const target = event.target;
+
+  const card = target.closest(".social-card");
+
+  if (!card) return;
+
+  animateBtn(card);
+});
+
 const headerSection = document.getElementById("header");
 
 const observerOptions = { threshold: 0 };
@@ -48,16 +65,15 @@ const mutationObserver = new MutationObserver((mutations) => {
     if (mutation.attributeName !== "active") return;
 
     const value = mutation.target.attributes.getNamedItem("active");
-    if (value) headerSection.classList.add("hide");
-    else headerSection.classList.remove("hide");
+    if (value) {
+      headerSection.classList.add("hide");
+    } else {
+      headerSection.classList.remove("hide");
+    }
   }
 });
 
 mutationObserver.observe(headerSection, {
   attributes: true,
   attributeFilter: ["active"],
-});
-
-heroSection.addEventListener("touchstart", () => {
-  headerSection.classList.remove("hide");
 });
